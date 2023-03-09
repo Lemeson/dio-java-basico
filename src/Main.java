e112fa7cbe2f00b29c559cb102c3a251a886ea9e
Lemeson <lemeson93@gmail.com>
2023-03-09 19:02:43 -0300

    final do projeto - uff

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,9 @@
+import java.time.LocalDate;
+
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
+import br.com.dio.desafio.dominio.Mentoria;
 
 public class Main{
     public static void main(String[] args) {
@@ -12,7 +17,48 @@ public class Main{
         curso2.setDescricao("descrecao curso java");
         curso2.setCargaHoraria(8);
 
-        System.out.println(curso1);
-        System.out.println(curso2);
+        Mentoria mentoria1 = new Mentoria();
+        mentoria1.setTitulo("titulo");
+        mentoria1.setDescricao("descricao mentoria java");
+        mentoria1.setData(LocalDate.now());
+
+        //System.out.println(curso1);
+      //  System.out.println(curso2);
+        //System.out.println(mentoria1);
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria1);
+
+        Dev devJoao  = new Dev();
+        devJoao.setNome("João");
+        devJoao.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos João" + devJoao.getConteudosInscritos());
+        
+        devJoao.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos Inscritos João" + devJoao.getConteudosInscritos());
+        System.out.println("Conteúdos Conlcuidos João" + devJoao.getConteudosConcluidos());
+        System.out.println("XP: " + devJoao.calcularTotalXp());
+
+        System.out.println("----------------");
+
+        Dev devLemeson  = new Dev();
+        devLemeson.setNome("Lemeson");
+        devLemeson.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Leme" + devLemeson.getConteudosInscritos());
+        devJoao.progredir();
+
+        System.out.println("-");
+        
+
+        System.out.println("Conteúdos Inscritos Leme" + devLemeson.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos Leme" + devLemeson.getConteudosConcluidos());
+
+
+        
     }
 }
